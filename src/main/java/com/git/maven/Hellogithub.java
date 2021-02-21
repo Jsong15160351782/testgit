ce1a154462d84fb31852ed862f90b4eddf7878b1
Koris <370012163@qq.com>
2021-02-21 12:33:24 +0800

    Koris66

---
+++ src/main/java/com/git/maven/Hellogithub.java
@@ -0,0 +1,10 @@
+package com.git.maven;
+
+public class Hellogithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("通过github克隆项目添加内容");
+	}
+
+}
