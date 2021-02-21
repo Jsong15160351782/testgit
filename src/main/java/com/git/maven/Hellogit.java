b0407a9a1272c9b87f2bb6979e92b2c9d7900653
Jsong0530 <87596856@163.com>
2021-02-21 11:00:25 +0800

    更新1

--- src/main/java/com/git/maven/Hellogit.java
+++ src/main/java/com/git/maven/Hellogit.java
@@ -5,6 +5,7 @@ public class Hellogit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          System.out.println("Hello gitMaven");
+	     System.out.println("更新1");
 	}
 
 }
