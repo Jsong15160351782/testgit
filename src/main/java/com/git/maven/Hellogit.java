e65dbc4ab87b88794818e7e9e82bed465c36ec7e
Jsong0530 <87596856@163.com>
2021-02-21 10:45:19 +0800

    添加文件

---
+++ src/main/java/com/git/maven/Hellogit.java
@@ -0,0 +1,10 @@
+package com.git.maven;
+
+public class Hellogit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("Hello gitMaven");
+	}
+
+}
